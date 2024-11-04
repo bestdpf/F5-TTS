2473348127c57987b05776872d1d734f54cf662f
pengfeiduan <pengfeiduan@tencent.com>
2024-11-05 07:10:55 +0800

    skip 7200

--- src/f5_tts/model/trainer.py
+++ src/f5_tts/model/trainer.py
@@ -297,7 +297,7 @@ class Trainer:
                     mel_lengths = batch["mel_lengths"]
 
                     total_length = mel_lengths.sum()
-                    if total_length > 3840:
+                    if total_length > 7200:
                         print(f'skip mel too long {total_length} in {global_step}')
                         continue
 
