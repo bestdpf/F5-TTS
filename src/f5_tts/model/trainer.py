d16824d4ae732f6700cae75cfedf8779ea3057d4
pengfeiduan <pengfeiduan@tencent.com>
2024-11-09 18:03:04 +0800

    lr issues

--- src/f5_tts/model/trainer.py
+++ src/f5_tts/model/trainer.py
@@ -247,7 +247,7 @@ class Trainer:
         #  accelerator.prepare() dispatches batches to devices;
         #  which means the length of dataloader calculated before, should consider the number of devices
         warmup_steps = (
-            self.num_warmup_updates * self.accelerator.num_processes
+            self.num_warmup_updates * self.accelerator.num_processes / self.grad_accumulation_steps
         )  # consider a fixed warmup steps while using accelerate multi-gpu ddp
         # otherwise by default with split_batches=False, warmup steps change with num_processes
         total_steps = len(train_dataloader) * self.epochs / self.grad_accumulation_steps
@@ -262,6 +262,7 @@ class Trainer:
         )  # actual steps = 1 gpu steps / gpus
         start_step = self.load_checkpoint()
         # start_step = 195600
+
         global_step = start_step
 
         if exists(resumable_with_seed):
