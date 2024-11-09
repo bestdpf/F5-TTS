26e94105978d5e6df8fe40f0e03bec869879cb18
pengfeiduan <pengfeiduan@tencent.com>
2024-11-09 18:18:42 +0800

    reset scheduler

--- src/f5_tts/model/trainer.py
+++ src/f5_tts/model/trainer.py
@@ -182,8 +182,8 @@ class Trainer:
 
             self.accelerator.unwrap_model(self.model).load_state_dict(checkpoint["model_state_dict"])
             self.accelerator.unwrap_model(self.optimizer).load_state_dict(checkpoint["optimizer_state_dict"])
-            if self.scheduler:
-                self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+            # if self.scheduler:
+            #     self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
             step = checkpoint["step"]
         else:
             checkpoint["model_state_dict"] = {
