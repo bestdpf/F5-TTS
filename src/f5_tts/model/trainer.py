63aa3d49672110d9c817d6d0c9cb9477211b7161
pengfeiduan <pengfeiduan@tencent.com>
2024-11-09 19:21:42 +0800

    dump scheduler

--- src/f5_tts/model/trainer.py
+++ src/f5_tts/model/trainer.py
@@ -244,6 +244,9 @@ class Trainer:
         else:
             raise ValueError(f"batch_size_type must be either 'sample' or 'frame', but received {self.batch_size_type}")
 
+        start_step = self.load_checkpoint()
+        # start_step = 195600
+        global_step = start_step
 
         #  accelerator.prepare() dispatches batches to devices;
         #  which means the length of dataloader calculated before, should consider the number of devices
@@ -262,12 +265,7 @@ class Trainer:
             train_dataloader, self.scheduler
         )  # actual steps = 1 gpu steps / gpus
 
-        print(f'dump scheduler {self.scheduler.state_dict()}')
-
-        start_step = self.load_checkpoint()
-        # start_step = 195600
-        global_step = start_step
-
+        # print(f'dump scheduler {self.scheduler.state_dict()}')
 
 
         if exists(resumable_with_seed):
