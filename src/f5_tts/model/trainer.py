8ca888a3ece4b2f6961d35a9165ca08107fa22da
pengfeiduan <pengfeiduan@tencent.com>
2024-11-10 19:14:14 +0800

    reset_scheduler

--- src/f5_tts/model/trainer.py
+++ src/f5_tts/model/trainer.py
@@ -182,8 +182,8 @@ class Trainer:
 
             self.accelerator.unwrap_model(self.model).load_state_dict(checkpoint["model_state_dict"], strict=False)
             self.accelerator.unwrap_model(self.optimizer).load_state_dict(checkpoint["optimizer_state_dict"])
-            if self.scheduler:
-                self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
+            # if self.scheduler:
+            #     self.scheduler.load_state_dict(checkpoint["scheduler_state_dict"])
             step = checkpoint["step"]
         else:
             checkpoint["model_state_dict"] = {
@@ -244,6 +244,9 @@ class Trainer:
         else:
             raise ValueError(f"batch_size_type must be either 'sample' or 'frame', but received {self.batch_size_type}")
 
+        start_step = self.load_checkpoint()
+        # start_step = 195600
+        global_step = start_step
         #  accelerator.prepare() dispatches batches to devices;
         #  which means the length of dataloader calculated before, should consider the number of devices
         warmup_steps = (
@@ -260,9 +263,7 @@ class Trainer:
         train_dataloader, self.scheduler = self.accelerator.prepare(
             train_dataloader, self.scheduler
         )  # actual steps = 1 gpu steps / gpus
-        start_step = self.load_checkpoint()
-        # start_step = 195600
-        global_step = start_step
+
 
         if exists(resumable_with_seed):
             orig_epoch_step = len(train_dataloader)
